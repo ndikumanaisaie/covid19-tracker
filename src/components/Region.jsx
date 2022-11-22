d94bd227c92459cca10a64d917b10afc6df342f0
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-22 23:37:41 +0100

    Add style to cards

--- src/components/Region.jsx
+++ src/components/Region.jsx
@@ -1,18 +1,22 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
 const Region = ({ data, cardClass }) => {
-  const { ActiveCases, Country, id } = data;
+  const {
+    ActiveCases, Country, id, flagUrl,
+  } = data;
   return (
     <Link to={`/country-details/${id}`}>
       <Card className={cardClass}>
+        <Card.Img variant="top" src={flagUrl} style={{ width: '7rem', height: '7rem' }} />
         <Card.Body>
           <Card.Title>
             { Country }
           </Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
+          <Card.Subtitle className="mb-2">
             {ActiveCases}
             {' '}
             Cases
@@ -28,6 +32,7 @@ Region.propTypes = {
     ActiveCases: PropTypes.number,
     Country: PropTypes.string,
     id: PropTypes.string,
+    flagUrl: PropTypes.string,
   }).isRequired,
   cardClass: PropTypes.string.isRequired,
 };
--- src/pages/Regions.jsx
+++ src/pages/Regions.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { findFlagUrlByCountryName } from 'country-flags-svg';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 import { getCovidData } from '../redux/covidData/covidDataSlice';
-import Region from '../components/Region';
 import Spinner from '../components/Spinner';
-import isOdd from '../utils/utils';
+// import isOdd from '../utils/utils';
 
 const Regions = () => {
   const [category, setCategory] = useState('Africa');
@@ -19,14 +20,21 @@ const Regions = () => {
 
   const filteredCovidData = covidData.filter((data) => data.Country !== 'Total:' && data.Country !== 'World');
 
+  const countryWithFlag = filteredCovidData.map((data) => (
+    {
+      ...data,
+      flagUrl: findFlagUrlByCountryName(data.Country),
+    }
+  ));
+
   const Continents = ['North America', 'South America', 'Asia', 'Africa', 'Europe', 'Australia/Oceania'];
 
-  const dataByContinent = filteredCovidData.filter((data) => data.Continent === category);
+  const dataByContinent = countryWithFlag.filter((data) => data.Continent === category);
 
   const totalContinentCases = dataByContinent
     .reduce((acc, curr) => acc + curr.ActiveCases, 0);
 
-  console.log('totalContinentCases: ', totalContinentCases);
+  // const myClass = 'cardItem';
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -38,7 +46,7 @@ const Regions = () => {
   }
 
   return (
-    <div>
+    <div className="container-fluid">
       <h1>
         Covid-19 Tracker
       </h1>
@@ -46,6 +54,7 @@ const Regions = () => {
         aria-label="Select Continent"
         onChange={handleChange}
         name="category"
+        className="continent-select"
       >
         <option>Continents</option>
         {
@@ -54,7 +63,7 @@ const Regions = () => {
           ))
         }
       </Form.Select>
-      <Card>
+      <Card className="continent-card">
         <Card.Body>
           <Card.Title>
             { category }
@@ -66,11 +75,27 @@ const Regions = () => {
           </Card.Subtitle>
         </Card.Body>
       </Card>
-      {
-        dataByContinent.map((data, i) => (
-          <Region cardClass={isOdd(i) ? 'evenCards' : 'oddCards'} key={data.id} data={data} />
-        ))
-      }
+      <div className="card-container">
+        {
+          dataByContinent.map((data) => (
+            <Link className="card-item" to={`/country-details/${data.id}`} key={data.useDispatch}>
+              <div>
+                <Card.Img variant="top" src={data.flagUrl} style={{ width: '7rem', height: '7rem' }} />
+                <Card.Body>
+                  <Card.Title>
+                    { data.Country }
+                  </Card.Title>
+                  <Card.Subtitle className="mb-2">
+                    {data.ActiveCases}
+                    {' '}
+                    Cases
+                  </Card.Subtitle>
+                </Card.Body>
+              </div>
+            </Link>
+          ))
+        }
+      </div>
     </div>
   );
 };
