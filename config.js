cd76a70fbf91fcb4bdfea8b25e50f148415419c8
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-23 17:39:54 +0100

    Move the config to the source folder

--- config.js
+++
@@ -1,2 +0,0 @@
-const REACT_APP_RAPIDAPI_KEY = '4b45196971mshda8f4f3676c8c3ap1cb460jsncf5cf53c390a';
-export default REACT_APP_RAPIDAPI_KEY;
--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+import apiKey from '../config';
+
 const API = axios.create({
   method: 'GET',
   baseURL: 'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/',
   headers: {
-    'X-RapidAPI-Key': '4b45196971mshda8f4f3676c8c3ap1cb460jsncf5cf53c390a',
+    'X-RapidAPI-Key': apiKey,
     'X-RapidAPI-Host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
   },
 });
