1f98bb64e5f2391b0f8b09c83f8eb2a59f6a6d94
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-23 17:44:26 +0100

    Fix linter error

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-import apiKey from '../config';
+import REACT_APP_RAPIDAPI_KEY from '../config';
 
 const API = axios.create({
   method: 'GET',
   baseURL: 'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/',
   headers: {
-    'X-RapidAPI-Key': apiKey,
+    'X-RapidAPI-Key': REACT_APP_RAPIDAPI_KEY,
     'X-RapidAPI-Host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
   },
 });
